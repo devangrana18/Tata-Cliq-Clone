79a6c6f367aac4245e829097efbc79de45526109
Devang18 <137139535+devangrana18@users.noreply.github.com>
2024-10-14 17:33:53 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const productsRoute = require("../backend/routes/product.routes");
-const userRouts = require("../backend/routes/auth.routes")
-const category = require("../backend/routes/categories.routes")
-const BagsRoutes = require("../backend/routes/bag.routes")
+const productsRoute = require("./routes/product.routes");
+const userRouts = require("./routes/auth.routes")
+const category = require("./routes/categories.routes")
+const BagsRoutes = require("./routes/bag.routes")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,7 +19,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-
+app.get("/",(req,res)=>{
+  res.send("connected")
+})
 app.use("/api/products", productsRoute);
 app.use("/api/auth",userRouts)
 app.use("/categories", category)
@@ -28,7 +30,7 @@ app.get('/api/products/search', async (req, res) => {
   const searchQuery = req.query.q;
 
   try {
-    const products = await Product.find({
+    const products = await products.find({
       name: { $regex: searchQuery, $options: 'i' },
     });
 
